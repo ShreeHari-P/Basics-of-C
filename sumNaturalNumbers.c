1fe68f9b3bd07f40770b1ff94caf108479bf97b0
Shree Hari P <shreehariparthiban159@gmail.com>
2025-10-17 06:24:34 +0530

    Sum of Natural Number up to N

---
+++ sumNaturalNumbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,sum = 0;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+
+    for(int i = 1; i <= n; i++){
+        sum += i;
+    }
+    printf("The sum of N numbers is %d\n",sum);
+    return 0;
+}
